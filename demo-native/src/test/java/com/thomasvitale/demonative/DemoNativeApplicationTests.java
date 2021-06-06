62ae38a85401ca30965df26cc93f6a500847fb0a
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2021-06-06 15:37:08 +0200

    Add native demo

---
+++ demo-native/src/test/java/com/thomasvitale/demonative/DemoNativeApplicationTests.java
@@ -0,0 +1,24 @@
+package com.thomasvitale.demonative;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class DemoServiceApplicationTests {
+
+	@Autowired
+	private WebTestClient webClient;
+
+	@Test
+	void whenGetBooksThenReturn() {
+		webClient
+				.get().uri("/")
+				.exchange()
+				.expectStatus().is2xxSuccessful()
+				.expectBody(String.class).isEqualTo("Spring Boot in the cloud and beyond!");
+	}
+
+}
