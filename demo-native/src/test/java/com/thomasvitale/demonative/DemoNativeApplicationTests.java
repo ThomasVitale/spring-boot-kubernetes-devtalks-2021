9a91c032667c196714a9a0b1d7c964b80c54eb5a
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2021-06-06 18:32:31 +0200

    Update build config

--- demo-native/src/test/java/com/thomasvitale/demonative/DemoNativeApplicationTests.java
+++ demo-native/src/test/java/com/thomasvitale/demonative/DemoNativeApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class DemoServiceApplicationTests {
+class DemoNativeApplicationTests {
 
 	@Autowired
 	private WebTestClient webClient;
